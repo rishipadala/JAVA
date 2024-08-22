46dd3bad88853f4ec96180864b5374a5029066b3
rishii_20 <rvpadala20@gmail.com>
2024-08-22 19:07:44 +0530

    Constructor Overloading

---
+++ GuessTheNumberEx3OOP.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class game{
+    int number;
+    int NoofGuesses;
+    int userInput;
+
+
+       game(){
+        Random rand = new Random();
+        this.number = rand.nextInt(0,100);
+    }
+
+    void takeUserInput() {
+        System.out.println("Guess The number : ");
+        Scanner sc = new Scanner(System.in);
+        userInput = sc.nextInt();
+    }
+    boolean isCorrectNumber(){
+           NoofGuesses++;
+           if (userInput == number){
+               System.out.format("Congratulations ,You Guessed the number %d Right!\n You guessed it in %d no.of attempts",number,NoofGuesses);
+               return true;
+           }
+           else if(userInput<=number){
+               System.out.println("Number you guessed is Lesser than the Actual Guessed Number,Try Higher!");
+           }
+           else if (userInput>=number) {
+               System.out.println("Number you guessed is Higher than the Actual Guessed Number,Try Lower!");
+           }
+           return false;
+    }
+
+    public void setNoofGuesses(int noofGuesses) {
+        NoofGuesses = noofGuesses;
+    }
+
+    public int getNoofGuesses() {
+        return NoofGuesses;
+    }
+}
+
+public class GuessTheNumberEx3OOP {
+    public static void main(String[] args) {
+        game g = new game();
+        boolean b = false;
+        while (!b){
+            g.takeUserInput();
+            b = g.isCorrectNumber();
+        }
+    }
+
+}
