b5e9ed3daaf4f47a3fd058799bf24302460ea24d
rishii_20 <rvpadala20@gmail.com>
2025-01-11 18:02:44 +0530

    Advance 2 - anonymous,lamda,javadocs,generics

---
+++ Docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.company","c":"javadoc","l":"add(int, int)","u":"add(int,int)"},{"p":"com.company","c":"javadoc","l":"javadoc()","u":"%3Cinit%3E()"},{"p":"com.company","c":"javadoc","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
